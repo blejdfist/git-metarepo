7837ac3dfaadb55cefe1f51fad348bcfef508de2
Jim Persson <jim@nurd.se>
2020-08-31 21:33:32 +0200

    Add methods that format and return ui elements

--- metarepo/ui.py
+++ metarepo/ui.py
@@ -8,12 +8,28 @@ CROSS_MARK = "\u2717"
 BULLET = "\u2022"
 
 
+def format_info(message):
+    """
+    Format information message
+    :param message: Message to format
+    """
+    return f"{Fore.BLUE}{message}{Style.RESET_ALL}"
+
+
 def info(message):
     """
     Print information message
     :param message: Message to print
     """
-    print(f"{Fore.BLUE}{message}{Style.RESET_ALL}")
+    print(format_info(message))
+
+
+def format_error(message):
+    """
+    Format error message
+    :param message: Message to format
+    """
+    return f"{Fore.RED}{message}{Style.RESET_ALL}"
 
 
 def error(message):
@@ -21,14 +37,14 @@ def error(message):
     Print error message
     :param message: Message to print
     """
-    print(f"{Fore.RED}{message}{Style.RESET_ALL}")
+    print(format_error(message))
 
 
-def item_symbol(symbol: str, message: str, *attributes: Union[Tuple[str, str], str]):
+def format_item_symbol(symbol: str, message: str, *attributes: Union[Tuple[str, str], str]):
     """
-    Format and print one item with custom symbol
+    Format one item with custom symbol
     :param symbol: Symbol
-    :param message: Message to print
+    :param message: Message to format
     :param attributes: List of extra attributes to format
     """
     formatted_attributes = []
@@ -38,31 +54,63 @@ def item_symbol(symbol: str, message: str, *attributes: Union[Tuple[str, str], s
         else:
             formatted_attributes.append(f"{attribute[0]}:{Fore.CYAN}{attribute[1]}{Style.RESET_ALL}")
 
-    print(f"  {symbol}{Style.RESET_ALL} {message} ({' '.join(formatted_attributes)})")
+    if formatted_attributes:
+        attributes = f" ({' '.join(formatted_attributes)})"
+    else:
+        attributes = ""
+
+    return f"  {symbol}{Style.RESET_ALL} {message}{attributes}"
+
+
+def format_item(message: str, *attributes: Union[Tuple[str, str], str]):
+    """
+    Format one list item
+    :param message: Message to format
+    :param attributes: List of extra attributes to format
+    """
+    return format_item_symbol(f"{Fore.LIGHTBLUE_EX}{BULLET}", message, *attributes)
+
+
+def format_item_ok(message: str, *attributes: Union[Tuple[str, str], str]):
+    """
+    Format one successful list item
+    :param message: Message to format
+    :param attributes: List of extra attributes to format
+    """
+    return format_item_symbol(f"{Fore.GREEN}{CHECK_MARK}", message, *attributes)
+
+
+def format_item_error(message: str, err: str):
+    """
+    Format one error list item
+    :param message: Message to format
+    :param err: Error details
+    """
+    return format_item_symbol(f"{Fore.RED}{CROSS_MARK}", message, f"{Fore.RED}{err}{Fore.RESET}")
 
 
 def item(message: str, *attributes: Union[Tuple[str, str], str]):
     """
-    Format and print one list item
-    :param message: Message to print
+    Print one list item
+    :param message: Message to format
     :param attributes: List of extra attributes to format
     """
-    item_symbol(f"{Fore.LIGHTBLUE_EX}{BULLET}", message, *attributes)
+    print(format_item(message, *attributes))
 
 
 def item_ok(message: str, *attributes: Union[Tuple[str, str], str]):
     """
-    Format and print one successful list item
-    :param message: Message to print
+    Print one successful list item
+    :param message: Message to format
     :param attributes: List of extra attributes to format
     """
-    item_symbol(f"{Fore.GREEN}{CHECK_MARK}", message, *attributes)
+    print(format_item_ok(message, *attributes))
 
 
 def item_error(message: str, err: str):
     """
-    Format and print one error list item
-    :param message: Message to print
+    Print one error list item
+    :param message: Message to format
     :param err: Error details
     """
-    item_symbol(f"{Fore.RED}{CROSS_MARK}", message, f"{Fore.RED}{err}{Fore.RESET}")
+    print(format_item_error(message, err))
